44d23982e3a4dfc4a2d64d91091e733565424146
gumiranda <imartytk@hotmail.com>
2020-07-26 10:28:00 -0300

    configurando onesignal

--- mobile/App.js
+++ mobile/App.js
@@ -1,17 +1,25 @@
 import './src/config/reactotronConfig';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider, useSelector} from 'react-redux';
 import {StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {darken} from 'polished';
+import OneSignal from 'react-native-onesignal';
 import {appStore, appPersistor} from './src/appStore/appStore';
 import createRouter from './routes';
 import {appColors} from './src/utils/appColors';
+import appConstants from './src/utils/appConstants';
 
 function DevDoido() {
   const signed = useSelector(state => state.auth.signed);
   const Routes = createRouter(signed);
+  const onIds = data => {};
+  useEffect(() => {
+    OneSignal.init(appConstants.ONE_SIGNAL_KEY);
+    OneSignal.addEventListener('ids', onIds);
+    return () => OneSignal.removeEventListener('ids', onIds);
+  }, []);
   return <Routes />;
 }
 
